2b1ba01638cb0a893f3777962847e0eb39546054
krewie <ionescu.kristian@gmail.com>
2018-10-11 22:18:27 +0200

    testing push

--- downloadTrainingData.py
+++ downloadTrainingData.py
@@ -10,7 +10,7 @@ def download():
             "http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz",
             "http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz",
             "http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz"]
-
+#lul
     for url in urls:
         filename = url.split("/")[-1]
 
